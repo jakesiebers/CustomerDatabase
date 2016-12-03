33734c9994d4d7daf121d5dba3af490f43a16e17
Jake Siebers <jakesiebers@gmail.com>
2016-12-02 20:45:06 -0700

    Records Endpoint

--- server.js
+++ server.js
@@ -3,14 +3,52 @@
 // NPM Modules
 const express = require('express');
 
+// Internal Modules
+const Parser = require('./parser');
+
 // Express Initialization
 var app = express();
 
 // REST Endpoints
 
-app.get('/', function (req, res) {
+app.post('/records', function (req, res) {
 
-  res.send('Hello World!');
+  var failures = 0;
+  var successes = 0;
+
+  req.setEncoding('utf8');
+
+  var items = req.pipe(new Parser());
+
+  items.on('data', (data) => {
+
+    if(typeof data === 'string'){
+
+      failures++;
+
+    }else{
+
+      successes++;
+
+    }
+
+  });
+
+  items.on('end', () => {
+
+    var response = {};
+
+    if(failures){
+      response.failures = failures;
+    } else {
+      response.success = true;
+    }
+    if(successes) response.successes = successes;
+
+    res.setHeader("Content-Type", "text/json");
+    res.send(JSON.stringify(response));
+
+  });
 
 });
 
